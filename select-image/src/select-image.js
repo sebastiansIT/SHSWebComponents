85591aac0236ab5b6830bf7e808303cb45fe0a0b
Sebastian <sebastian@human-injection.de>
2019-12-08 18:04:18 +0100

    Format Licence Note as JSDoc comment to preserve it when minifing with Terser.

--- select-image/src/select-image.js
+++ select-image/src/select-image.js
@@ -1,21 +1,22 @@
-/*  Copyright 2019 Sebastian Spautz
-
-    This File is Part of "SITWebComponents"
-    <https://github.com/sebastiansIT/SHSWebComponents>.
-
-    SITWebComponents is free software: you can redistribute it and/or modify
-    it under the terms of the Lesser GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    SITWebComponents is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    Lesser GNU General Public License for more details.
-
-    You should have received a copy of the Lesser GNU General Public License
-    along with SITWebComponents. If not, see <http://www.gnu.org/licenses/>.
-*/
+/** @license
+ * Copyright 2019 Sebastian Spautz
+ *
+ * This File is Part of "SITWebComponents"
+ * <https://github.com/sebastiansIT/SHSWebComponents>.
+ *
+ * SITWebComponents is free software: you can redistribute it and/or modify
+ * it under the terms of the Lesser GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * SITWebComponents is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * Lesser GNU General Public License for more details.
+ *
+ * You should have received a copy of the Lesser GNU General Public License
+ * along with SITWebComponents. If not, see <http://www.gnu.org/licenses/>.
+ */
 
 /** The base for all HTML elements.
  * @external HTMLElement
