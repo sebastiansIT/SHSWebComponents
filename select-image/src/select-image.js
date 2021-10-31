5f3acd9d4754217e8caa02a0ef980b5b10d4ecab
SebastiansIT <sebastian@human-injection.de>
2021-10-31 11:59:33 +0100

    Change disabled state of reset button when value attribute is changed from outside.

--- select-image/src/select-image.js
+++ select-image/src/select-image.js
@@ -1,5 +1,5 @@
 /** @license LGPL-3.0-or-later
- * Copyright 2019, 2020 Sebastian Spautz
+ * Copyright 2019, 2020 Sebastian Spautz.
  *
  * This File is Part of "SITWebComponents"
  * <https://github.com/sebastiansIT/SHSWebComponents>.
@@ -255,8 +255,10 @@ class SelectImageElement extends HTMLElement {
       case 'value':
         if (!newVal) {
           this._image.removeAttribute('src')
+          this._revertButton.disabled = true
         } else {
           this._image.src = newVal
+          this._revertButton.disabled = false
         }
         break
       case 'alt':
